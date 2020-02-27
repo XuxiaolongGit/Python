5a43eba2ce126e4c464e03fbf2a73591759fcb96
xxl <xxl46071496@163.com>
2020-02-27 16:59:10 +0800

    change

--- src/src/settings.py
+++ src/src/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_9jlqy5sg3)1!sidn+h!2vdog9sa&efnfpkgdcw&5-iswe&gts'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
